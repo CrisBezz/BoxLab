626005196e1006e74c5d7fbdd923f1b230b4006c
Bezzo <115067958+CrisBezz@users.noreply.github.com>
2026-08-27 08:58:29 +1000

    BoxLab v0.14 install bevel topology

--- src/loose-bootstrap.js
+++ src/loose-bootstrap.js
@@ -1,6 +1,7 @@
 import { EditableMesh } from './mesh.js';
 import { installLooseTopology } from './loose-topology.js';
 import { installBridgeTopology } from './bridge-topology.js';
+import { installBevelTopology } from './bevel-topology.js?v=0.14';
 import { installFaceTransform } from './face-transform.js?v=0.12';
 import { installFaceRegion } from './face-region.js?v=0.13';
 import './selection-transform-state.js?v=0.12';
@@ -8,5 +9,6 @@ import './face-pick-repair.js?v=0.12';
 
 installLooseTopology(EditableMesh);
 installBridgeTopology(EditableMesh);
+installBevelTopology(EditableMesh);
 installFaceTransform();
 installFaceRegion(EditableMesh);
