f6c53f564dfee0b259ab6fb8acf5010de879a68c
Bezzo <115067958+CrisBezz@users.noreply.github.com>
2026-08-26 21:54:17 +1000

    BoxLab v0.11.2 load face picker repair before core

--- src/loose-bootstrap.js
+++ src/loose-bootstrap.js
@@ -1,6 +1,7 @@
 import { EditableMesh } from './mesh.js';
 import { installLooseTopology } from './loose-topology.js';
 import { installBridgeTopology } from './bridge-topology.js';
+import './face-pick-repair.js?v=0.11.2';
 
 installLooseTopology(EditableMesh);
 installBridgeTopology(EditableMesh);
