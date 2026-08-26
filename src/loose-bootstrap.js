9408ea04ee97ff96da75c0f38ffad5ef4c4a1897
Bezzo <115067958+CrisBezz@users.noreply.github.com>
2026-08-26 21:15:16 +1000

    BoxLab v0.11 install bridge topology

--- src/loose-bootstrap.js
+++ src/loose-bootstrap.js
@@ -1,4 +1,6 @@
 import { EditableMesh } from './mesh.js';
 import { installLooseTopology } from './loose-topology.js';
+import { installBridgeTopology } from './bridge-topology.js';
 
 installLooseTopology(EditableMesh);
+installBridgeTopology(EditableMesh);
