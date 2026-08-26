b28e5c1576485dc64b81f8035bcc032e0386810f
Bezzo <115067958+CrisBezz@users.noreply.github.com>
2026-08-26 21:58:14 +1000

    BoxLab v0.11.3 wire Face Region tools

--- src/loose-bootstrap.js
+++ src/loose-bootstrap.js
@@ -1,7 +1,11 @@
 import { EditableMesh } from './mesh.js';
 import { installLooseTopology } from './loose-topology.js';
 import { installBridgeTopology } from './bridge-topology.js';
-import './face-pick-repair.js?v=0.11.2';
+import { installFaceTransform } from './face-transform.js?v=0.11.3';
+import { installFaceRegion } from './face-region.js?v=0.11.3';
+import './face-pick-repair.js?v=0.11.3';
 
 installLooseTopology(EditableMesh);
 installBridgeTopology(EditableMesh);
+installFaceTransform();
+installFaceRegion(EditableMesh);
