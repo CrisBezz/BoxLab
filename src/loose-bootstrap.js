428bbade060ed9a33266bbc75201e0932554f9c9
Bezzo <115067958+CrisBezz@users.noreply.github.com>
2026-08-26 22:01:10 +1000

    BoxLab v0.11.3 wire transform selection state

--- src/loose-bootstrap.js
+++ src/loose-bootstrap.js
@@ -3,6 +3,7 @@ import { installLooseTopology } from './loose-topology.js';
 import { installBridgeTopology } from './bridge-topology.js';
 import { installFaceTransform } from './face-transform.js?v=0.11.3';
 import { installFaceRegion } from './face-region.js?v=0.11.3';
+import './selection-transform-state.js?v=0.11.3';
 import './face-pick-repair.js?v=0.11.3';
 
 installLooseTopology(EditableMesh);
