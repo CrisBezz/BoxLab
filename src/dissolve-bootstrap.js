eb44181f90d7027ca8b1f69c3917e4ce3fd85110
Bezzo <115067958+CrisBezz@users.noreply.github.com>
2026-08-27 10:56:48 +1000

    BoxLab v0.15 load updated dissolve topology

--- src/dissolve-bootstrap.js
+++ src/dissolve-bootstrap.js
@@ -1,4 +1,4 @@
 import { EditableMesh } from './mesh.js?v=0.12';
-import { installDissolveTopology } from './dissolve-topology.js?v=0.12';
+import { installDissolveTopology } from './dissolve-topology.js?v=0.15';
 
 installDissolveTopology(EditableMesh);
