be1cc68ef58f9126c0fcc46802d9be3618ebfb26
Bezzo <115067958+CrisBezz@users.noreply.github.com>
2026-08-27 14:26:05 +1000

    BoxLab v0.18.3 install bevel selection routing

--- src/loose-bootstrap.js
+++ src/loose-bootstrap.js
@@ -3,6 +3,7 @@ import { installLooseTopology } from './loose-topology.js';
 import { installBridgeTopology } from './bridge-topology.js';
 import { installBevelTopology } from './bevel-topology.js?v=0.18';
 import { installGeneralEdgeBevelTopology } from './general-edge-bevel-topology.js?v=0.18.2';
+import { installBevelSelection } from './bevel-selection.js?v=0.18.3';
 import { installFaceTransform } from './face-transform.js?v=0.12';
 import { installFaceRegion } from './face-region.js?v=0.13';
 import './selection-transform-state.js?v=0.12';
@@ -12,6 +13,7 @@ installLooseTopology(EditableMesh);
 installBridgeTopology(EditableMesh);
 installBevelTopology(EditableMesh);
 installGeneralEdgeBevelTopology(EditableMesh);
+installBevelSelection(EditableMesh);
 installFaceTransform();
 installFaceRegion(EditableMesh);
 
