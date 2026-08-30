4f5c495ad986e32dd7d0fedfdafab73678c69276
Bezzo <115067958+CrisBezz@users.noreply.github.com>
2026-08-30 13:54:40 +1000

    BoxLab v0.32.19 publish offset loop drag fix

--- src/loose-bootstrap.js
+++ src/loose-bootstrap.js
@@ -33,4 +33,4 @@ installMultiVertexBevelTopology(EditableMesh);
 installFaceTransform();
 installFaceRegion(EditableMesh);
 
-import('./loop-offset.js?v=0.32.18');
+import('./loop-offset.js?v=0.32.19');
