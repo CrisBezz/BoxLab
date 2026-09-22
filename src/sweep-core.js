f4e3397b87902b9845bc983b6cdfd7ab3c3bedff
Bezzo <115067958+CrisBezz@users.noreply.github.com>
2026-09-22 14:08:20 +1000

    Advance Sweep core runtime stamp to v0.36.18.412

--- src/sweep-core.js
+++ src/sweep-core.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 import {EditableMesh} from './mesh.js';
 
-const VERSION='0.36.18.411';
+const VERSION='0.36.18.412';
 const EPS=1e-7;
 
 function cleanPath(points=[]){
