b4179f21bde1d94ee246283c9c24d70cbc380303
Bezzo <115067958+CrisBezz@users.noreply.github.com>
2026-09-24 12:54:17 +1000

    Preserve facegroups through SubD v0.36.18.445

--- src/subdivision.js
+++ src/subdivision.js
@@ -95,12 +95,13 @@ function catmullClarkStep(input){
     resultVertices.push(p.clone());
   });
 
-  const newFaces=[];
+  const newFaces=[],newFaceGroups=[];
   oldFaces.forEach((face,fi)=>{
     const fp=faceIndexMap.get(fi);
     for(let i=0;i<face.length;i++){
       const v=face[i],prev=face[(i-1+face.length)%face.length],next=face[(i+1)%face.length];
       newFaces.push([v,edgeIndexMap.get(edgeKey(v,next)),fp,edgeIndexMap.get(edgeKey(prev,v))]);
+      newFaceGroups.push(mesh.faceGroups?.[fi]??null);
     }
   });
 
@@ -113,7 +114,7 @@ function catmullClarkStep(input){
     nextCreases.set(edgeKey(ep,edge.b),w);
   });
 
-  return new EditableMesh(resultVertices,newFaces,nextCreases);
+  return new EditableMesh(resultVertices,newFaces,nextCreases,newFaceGroups);
 }
 
 export function catmullClark(input){return catmullClarkStep(input);}
